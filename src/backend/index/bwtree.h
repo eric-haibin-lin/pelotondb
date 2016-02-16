7eb8dfb78c00ab47651940966a5e2a9fa2507f75
eric-haibin-lin <linhaibin.eric@gmail.com>
2016-02-16 16:15:43 -0500

    clang code

--- src/backend/index/bwtree.h
+++ src/backend/index/bwtree.h
@@ -13,79 +13,68 @@
 #pragma once
 #include "backend/common/types.h"
 
-
 namespace peloton {
 namespace index {
-
 
 typedef uint32_t LPID;
 
 template <typename KeyType, typename ValueType, class KeyComparator>
 class BWTree {
-private:
+ private:
   LPID root;
-  std::map<uint32_t, BWTree**> mapping_table_;
+  std::map<uint32_t, BWTree **> mapping_table_;
 
-public:
-  bool InsertEntry(const storage::Tuple *key,
-      const ItemPointer location);
+ public:
+  bool InsertEntry(const storage::Tuple *key, const ItemPointer location);
 
   bool DeleteEntry(const storage::Tuple *key, const ItemPointer location);
   std::vector<ItemPointer> Scan(const std::vector<Value> &values,
                                 const std::vector<oid_t> &key_column_ids,
                                 const std::vector<ExpressionType> &expr_types,
-                                const ScanDirectionType& scan_direction);
+                                const ScanDirectionType &scan_direction);
 
   std::vector<ItemPointer> ScanAllKeys();
 
   std::vector<ItemPointer> ScanKey(const storage::Tuple *key);
 };
 
-
 // Look up the stx btree interface for background.
 // peloton/third_party/stx/btree.h
 template <typename KeyType, typename ValueType, class KeyComparator>
 class BWTreeNode {
+ protected:
+  std::map<uint32_t, BWTree **> *mapping_table_;
 
-protected:
-  std::map<uint32_t, BWTree**> *mapping_table_;
-
-public:
-
-  BWTreeNode(std::map<uint32_t, BWTree**> *mapping_table)
-    : mapping_table_ (mapping_table){};
+ public:
+  BWTreeNode(std::map<uint32_t, BWTree **> *mapping_table)
+      : mapping_table_(mapping_table){};
   virtual bool InsertEntry(const storage::Tuple *key,
-      const ItemPointer location)=0;
+                           const ItemPointer location) = 0;
 
-  virtual bool DeleteEntry(const storage::Tuple *key, const ItemPointer location);
+  virtual bool DeleteEntry(const storage::Tuple *key,
+                           const ItemPointer location);
   std::vector<ItemPointer> Scan(const std::vector<Value> &values,
                                 const std::vector<oid_t> &key_column_ids,
                                 const std::vector<ExpressionType> &expr_types,
-                                const ScanDirectionType& scan_direction)=0;
+                                const ScanDirectionType &scan_direction) = 0;
 
-  virtual std::vector<ItemPointer> ScanAllKeys()=0;
+  virtual std::vector<ItemPointer> ScanAllKeys() = 0;
 
-  virtual std::vector<ItemPointer> ScanKey(const storage::Tuple *key)=0;
+  virtual std::vector<ItemPointer> ScanKey(const storage::Tuple *key) = 0;
 
-  virtual ~BWTreeNode()=0;
-
+  virtual ~BWTreeNode() = 0;
 };
 
 template <typename KeyType, typename ValueType, class KeyComparator>
 class IPage : public BWTreeNode<KeyType, ValueType, KeyComparator> {
-
-private:
+ private:
   std::pair<KeyType, LPID> children_map_[];
-
 };
-
 
 template <typename KeyType, typename ValueType, class KeyComparator>
 class Delta : public BWTreeNode<KeyType, ValueType, KeyComparator> {
-
-protected:
-  BWTreeNode * modified_node_;
-
+ protected:
+  BWTreeNode *modified_node_;
 };
 
 // TODO More delta classes such as
@@ -93,15 +82,11 @@ protected:
 
 template <typename KeyType, typename ValueType, class KeyComparator>
 class LPage : public BWTreeNode<KeyType, ValueType, KeyComparator> {
-
-private:
+ private:
   LPID left_sib_;
   LPID right_sib_;
   std::pair<KeyType, ItemPointer> locations_[];
-
 };
-
-
 
 }  // End index namespace
 }  // End peloton namespace
